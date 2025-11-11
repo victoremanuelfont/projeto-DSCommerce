2a1a101e648ce802a61d088685f21491b3ed5b6b
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-11 19:41:54 -0300

    Associação muitos para muitos entre Product e Category

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dscommerce.entities;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="tb_category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category(){
     }
@@ -27,5 +39,9 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 }
