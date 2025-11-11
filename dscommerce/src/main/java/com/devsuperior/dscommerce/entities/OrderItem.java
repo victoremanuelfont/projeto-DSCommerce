514707aa4bde9b240bf8628cc8cc8000da42ebce
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-11 20:18:24 -0300

    Muitos-para-Muitos com classe de associação

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
 
+    @EmbeddedId
     private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
